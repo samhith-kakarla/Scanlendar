714f91c399ece25a4a9e25ab4e964f2f17eef036
Samhith Kakarla <samhith.kakarla@gmail.com>
2021-07-11 01:54:05 -0400

    setup react routing

--- scan_frontend/src/App.js
+++ scan_frontend/src/App.js
@@ -1,10 +1,21 @@
+import React from 'react'; 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import Home from './pages/Home'; 
+import Dashboard from './pages/Dashboard'; 
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/dashboard" component={Dashboard} />
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
